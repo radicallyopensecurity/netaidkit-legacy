1e2532a8889fede3d1c554c26ea964eaeb125e29
Mart van Santen <mart@greenhost.nl>
2015-02-03 20:25:35 +0100

    Added some initial documentation (and to test git push)

--- nak-pkg/nak-web-0.1/netaidkit.c
+++ nak-pkg/nak-web-0.1/netaidkit.c
@@ -7,18 +7,23 @@ int main(int argc, char **argv)
     command *cmd;
     char **args;
     int cmd_status;
-    
+ 
+	/* We need a minimal an argument */   
     if (argc < 2) {
         fprintf(stderr, "Usage: %s COMMAND [ARGS]...\n", argv[0]);
         exit(-1);
     }
-    
+ 
+	/* Get the first command and check if this command exists */   
     cmd = get_command(argv[1]);
     if (cmd == NULL) {
         fprintf(stderr, "Unrecognized command.\n");
         exit(-1);
     }
     
+	/* The get_command() returns an object where arg_count is set.
+     * if we have not an exact match between expected arguments 
+     * and actual aguments, we exit this application */
     if ((argc - 2) != cmd->arg_count) {
         fprintf(stderr, "Invalid number of argument(s).\n");
         exit(-1);
