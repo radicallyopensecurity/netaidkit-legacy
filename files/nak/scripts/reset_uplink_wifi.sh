717b42115ee745705ba27ca626de131622dbb29d
mensoh <menso@xs4all.nl>
2015-03-09 15:04:26 +0100

    Added hardware disable client mode

---
+++ files/nak/scripts/reset_uplink_wifi.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# reset uplink wifi
+uci set wireless.@wifi-iface[0].disabled=1
+uci set wireless.@wifi-iface[0].ssid='';
+uci set wireless.@wifi-iface[0].encryption='';
+uci set wireless.@wifi-iface[0].key='';
+uci commit wireless
+wifi
+
