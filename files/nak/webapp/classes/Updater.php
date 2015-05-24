6fb028ce4e0a3697d482fe22f4ec45bea622de12
sj0rz <cryptosj0rz@gmail.com>
2015-05-24 04:57:15 +0200

    Latest images.

--- files/nak/webapp/classes/Updater.php
+++ files/nak/webapp/classes/Updater.php
@@ -39,21 +39,20 @@ class Updater
     
     public function downloadLatest()
     {
-        $latest_image = file_get_contents($this->_latestImageUrl);
+        file_put_contents($this->_localImagePath, fopen($this->_latestImageUrl, 'r'));
         
-        if (empty($latest_image))
+        if (!file_exists($this->_localImagePath))
             throw new Exception('Could not download latest image.');
-        
-        return $latest_image;
     }
     
-    public function validateSignature($data)
+    public function validateSignature()
     {
         if (!file_exists($this->_pubKeyFile))
             throw new Exception('Public key file not found.');
         
         $pubkey = file_get_contents($this->_pubKeyFile);
         
+        $data = file_get_contents($this->_localImagePath);
         $signature = substr($data, -64);
         $data = substr($data, 0, -64);
         $status = openssl_verify($data, $signature, $pubkey);
@@ -64,10 +63,8 @@ class Updater
         return false;
     }
     
-    public function performUpdate($image_data)
+    public function performUpdate()
     {   
-        file_put_contents($this->_localImagePath, $image_data);
-        
         NetAidManager::do_update($this->_localImagePath);
         
         return true;
--- files/nak/webapp/controllers/UpdateController.php
+++ files/nak/webapp/controllers/UpdateController.php
@@ -2,26 +2,26 @@
 
 class UpdateController extends Page
 {
-    protected $_allowed_actions = array('index', 'do_upgrade');
-    
+    protected $_allowed_actions = array('index', 'do_upgrade', 'download_status');                  
+
     public function init()
     {
         if ($_SESSION['logged_in'] != 1)
             $this->_redirect('/user/login');
     }
-    
+
     public function index()
     {
         $updater = new Updater();
         if (!$updater->updateAvailable())
             $this->_redirect('/admin/index');
-        
+
         $params = array();
-    
+
         $view = new View('update', $params);
         return $view->display();
     }
-    
+
     public function do_upgrade()
     {
         $request = $this->getRequest();
@@ -32,7 +32,7 @@ class UpdateController extends Page
 
                 if (!$updater->validateSignature($image_data))
                     die('INVALID SIGNATURE');
-                    
+
                 if ($updater->performUpdate($image_data))
                     die('Upgrading, please wait for the device to reboot.');
                 else
@@ -40,4 +40,19 @@ class UpdateController extends Page
             }
         }
     }
+
+    public function download_status()
+    {
+        die($this->_get_image_size());
+    }
+
+    protected function _get_image_size()
+    {
+        $image = '/tmp/latest_image';
+        if (!file_exists($image))
+            return '0'; 
+
+        return floor((filesize($image) / 7471108) * 100); 
+    }
 }
+
