ec1d7e0423cf618afb3ce28aad4bcf02769bc255
sj0rz <cryptosj0rz@gmail.com>
2016-01-22 16:40:36 +0100

    fix infinite feof loop in case server does not send EOF
    In some cases the server response might not end in EOF, which
    will cause the web interface to hang. Use stream_get_meta_data
    to detect if all bytes have been read.

--- files/nak/webapp/classes/NakdClient.php
+++ files/nak/webapp/classes/NakdClient.php
@@ -10,8 +10,8 @@ class NakdClient {
         $this->_conn = $this->_connect($this->_socketFile);
         if (!$this->_conn) {
             $view = new View('booting');
-            $view->display();   
-            die();         
+            $view->display();
+            die();
         }
     }
 
@@ -27,11 +27,20 @@ class NakdClient {
         @fclose($this->_conn);
     }
 
+    /* From http://php.net/feof */
+    protected function _safe_feof($fp, &$start = NULL) {
+        $start = microtime(true);
+        return feof($fp);
+    }
+
     protected function _sendCommand($command) {
         fwrite($this->_conn, $command);
         $response = '';
-        while (!feof($this->_conn))
-            $response .= fread($this->_conn, MAX_MSG_LEN);
+        while (!feof($this->_conn)) {
+            $response .= fread($this->_conn, MAX_MSG_LEN - strlen($response));
+            $stream_meta_data = stream_get_meta_data($this->_conn);
+            if ($stream_meta_data['unread_bytes'] <= 0) break;
+        }
 
         return $response;
     }
